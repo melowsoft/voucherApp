495570c8474dd3501777d66770cf75944673a8eb
Melowsoft <melowsworld@yahoo.co.uk>
2018-08-23 14:22:11 +0100

    new commit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,10 +3,7 @@ import { AUTH_USER, AUTH_ERROR } from "./types";
 
 export const signup = (formProps, callback) => async dispatch => {
   try {
-    const response = await axios.post(
-      "http://localhost:5000/signup",
-      formProps
-    );
+    const response = await axios.post("/signup", formProps);
 
     dispatch({ type: AUTH_USER, payload: response.data.token });
     localStorage.setItem("token", response.data.token);
@@ -18,10 +15,7 @@ export const signup = (formProps, callback) => async dispatch => {
 
 export const signin = (formProps, callback) => async dispatch => {
   try {
-    const response = await axios.post(
-      "http://localhost:5000/signin",
-      formProps
-    );
+    const response = await axios.post("/signin", formProps);
 
     dispatch({ type: AUTH_USER, payload: response.data.token });
     localStorage.setItem("token", response.data.token);
