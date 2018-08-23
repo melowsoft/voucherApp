b658d469b01982d466f7fb8926990b34612b44f3
Melowsoft <melowsworld@yahoo.co.uk>
2018-08-23 14:55:21 +0100

    new committ

--- routes/router.js
+++ routes/router.js
@@ -5,9 +5,6 @@ const passport = require("passport");
 const requireSignin = passport.authenticate("local", { session: false });
 
 module.exports = function(app) {
-  app.get("/", function(req, res) {
-    res.send({ message: "Welome" });
-  });
   app.post("/signin", requireSignin, Authentication.signin);
   app.post("/signup", Authentication.signup);
 };
